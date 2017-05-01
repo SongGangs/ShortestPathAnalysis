e06994df62c5a2fc95c7b3e18e80598ad9dcc2ce
SongGangs <sg951205@vip.qq.com>
2017-05-01 20:22:39 +0800

    sg 提交工程

---
+++ ShortestPathDemo/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ShortestPathDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Openfile = new System.Windows.Forms.Panel();
+            this.Btu_OpenFile = new System.Windows.Forms.Button();
+            this.Txt_FileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_Tools = new System.Windows.Forms.Panel();
+            this.Btu_ShortestPath = new System.Windows.Forms.Button();
+            this.Btu_Load = new System.Windows.Forms.Button();
+            this.panel_Draw = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel_Openfile.SuspendLayout();
+            this.panel_Tools.SuspendLayout();
+            this.panel_Draw.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_Openfile
+            // 
+            this.panel_Openfile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_Openfile.Controls.Add(this.Btu_OpenFile);
+            this.panel_Openfile.Controls.Add(this.Txt_FileName);
+            this.panel_Openfile.Controls.Add(this.label1);
+            this.panel_Openfile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Openfile.Location = new System.Drawing.Point(0, 0);
+            this.panel_Openfile.Name = "panel_Openfile";
+            this.panel_Openfile.Size = new System.Drawing.Size(801, 64);
+            this.panel_Openfile.TabIndex = 0;
+            // 
+            // Btu_OpenFile
+            // 
+            this.Btu_OpenFile.Location = new System.Drawing.Point(699, 20);
+            this.Btu_OpenFile.Name = "Btu_OpenFile";
+            this.Btu_OpenFile.Size = new System.Drawing.Size(75, 30);
+            this.Btu_OpenFile.TabIndex = 2;
+            this.Btu_OpenFile.Text = "打 开";
+            this.Btu_OpenFile.UseVisualStyleBackColor = true;
+            this.Btu_OpenFile.Click += new System.EventHandler(this.Btu_OpenFile_Click);
+            // 
+            // Txt_FileName
+            // 
+            this.Txt_FileName.Location = new System.Drawing.Point(164, 20);
+            this.Txt_FileName.Name = "Txt_FileName";
+            this.Txt_FileName.ReadOnly = true;
+            this.Txt_FileName.Size = new System.Drawing.Size(501, 25);
+            this.Txt_FileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件读取:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel_Tools
+            // 
+            this.panel_Tools.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Tools.Controls.Add(this.Btu_ShortestPath);
+            this.panel_Tools.Controls.Add(this.Btu_Load);
+            this.panel_Tools.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Tools.Location = new System.Drawing.Point(0, 64);
+            this.panel_Tools.Name = "panel_Tools";
+            this.panel_Tools.Size = new System.Drawing.Size(118, 569);
+            this.panel_Tools.TabIndex = 1;
+            // 
+            // Btu_ShortestPath
+            // 
+            this.Btu_ShortestPath.Location = new System.Drawing.Point(13, 358);
+            this.Btu_ShortestPath.Name = "Btu_ShortestPath";
+            this.Btu_ShortestPath.Size = new System.Drawing.Size(82, 30);
+            this.Btu_ShortestPath.TabIndex = 3;
+            this.Btu_ShortestPath.Text = "计 算";
+            this.Btu_ShortestPath.UseVisualStyleBackColor = true;
+            this.Btu_ShortestPath.Click += new System.EventHandler(this.Btu_ShortestPath_Click);
+            // 
+            // Btu_Load
+            // 
+            this.Btu_Load.Location = new System.Drawing.Point(12, 64);
+            this.Btu_Load.Name = "Btu_Load";
+            this.Btu_Load.Size = new System.Drawing.Size(82, 30);
+            this.Btu_Load.TabIndex = 0;
+            this.Btu_Load.Text = "加 载";
+            this.Btu_Load.UseVisualStyleBackColor = true;
+            this.Btu_Load.Click += new System.EventHandler(this.Btu_Load_Click);
+            // 
+            // panel_Draw
+            // 
+            this.panel_Draw.Controls.Add(this.pictureBox1);
+            this.panel_Draw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Draw.Location = new System.Drawing.Point(0, 0);
+            this.panel_Draw.Name = "panel_Draw";
+            this.panel_Draw.Size = new System.Drawing.Size(1248, 633);
+            this.panel_Draw.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1248, 633);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(801, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(447, 633);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.richTextBox);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(0, 45);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(447, 588);
+            this.panel6.TabIndex = 2;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.Size = new System.Drawing.Size(447, 588);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(447, 45);
+            this.panel5.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(447, 45);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "结 果 记 录";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 633);
+            this.Controls.Add(this.panel_Tools);
+            this.Controls.Add(this.panel_Openfile);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel_Draw);
+            this.Name = "Form1";
+            this.Text = "最短路径";
+            this.panel_Openfile.ResumeLayout(false);
+            this.panel_Openfile.PerformLayout();
+            this.panel_Tools.ResumeLayout(false);
+            this.panel_Draw.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Openfile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel_Tools;
+        private System.Windows.Forms.Panel panel_Draw;
+        private System.Windows.Forms.Button Btu_OpenFile;
+        private System.Windows.Forms.TextBox Txt_FileName;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button Btu_Load;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Btu_ShortestPath;
+    }
+}
+
---
+++ ShortestPathDemo/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortestPathDemo
+{
+    public partial class Form1 : Form
+    {
+        private static string filename=String.Empty;
+        private static string str = String.Empty;//文本记录的数据
+        private static int PointCounts = 0;//点数
+        private static int[,] Metro = null;//  邻接矩阵
+        private static Point[] point = null;
+        private static List<Point> PL = null;
+
+        private int[] distance; //用以每次查询存放数据
+        private int[] prev; //用以存储前一个最近顶点的下标
+        private ArrayList S; //S储存确定最短路径的顶点的下标
+        private ArrayList U; //U中存储尚未确定路径的顶点的下标
+        private bool[] Isfor;
+        //基本画图
+        private static Graphics graphics;
+        private static Bitmap bitmap;
+        private static Pen pen;
+        //private static Font font;
+       // private static Brush brush;
+        public Form1()
+        {
+            InitializeComponent();
+            //font = new Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular);
+            //brush = new SolidBrush(Color.Blue);
+            
+        }
+
+        private void Btu_OpenFile_Click(object sender, EventArgs e)
+        {
+            //打开一个文件选择框
+            OpenFileDialog ofg=new OpenFileDialog();
+            ofg.Filter = "TXTfile(*.txt)|*.txt|All files(*.*)|*.*"; //打开文件路径
+            ofg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofg.Multiselect = false;
+            if (ofg.ShowDialog() == DialogResult.OK)
+            {
+                filename = ofg.FileName;
+                Txt_FileName.Text = filename;
+            }
+
+        }
+
+        private void Btu_Load_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                MessageBox.Show("请先选择文件");
+                return;
+            }
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string content = sr.ReadLine();
+            if (!string.IsNullOrEmpty(content))
+            {
+                try
+                {
+                    if (content.Contains("PointCounts:"))
+                    {
+                        content = sr.ReadLine();
+                        if (!string.IsNullOrEmpty(content))
+                            PointCounts = Int32.Parse(content.Trim());
+                        content = sr.ReadLine();
+                        str = "点数：" + PointCounts;
+                    }
+                     if (content.Contains("Weight:"))
+                    {
+                        content = sr.ReadLine();
+                        str = str + "\n" + "权重：" + "\n";
+                        Metro = new int[PointCounts, PointCounts];
+                        int k = 0;
+                        string[] weight = new string[PointCounts];
+
+                        while (!string.IsNullOrEmpty(content))
+                        {
+                            weight = content.Split(',');
+                            for (int i = 0; i < PointCounts; i++)
+                            {
+                                str = str + weight[i] + "\t";
+                                Metro[k, i] = int.Parse(weight[i]);
+                            }
+                            content = sr.ReadLine();
+                            str = str + "\n";
+                            k++;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    str = "文本内容有错，请检查！";
+                }
+            }
+            fs.Close();
+            sr.Close();
+            richTextBox.Text = str;
+
+            PL = new List<Point>();
+            bitmap = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            AddPoint();
+            AddLine();
+        }
+
+
+        private void AddPoint()
+        {
+            while (PL.Count<PointCounts)
+            {
+                Point p = Random_Point();
+                if (!PL.Contains(p))
+                {
+                    PL.Add(p);
+                    DrawPoint(p, "V" + PL.Count);
+                }
+            }
+        }
+
+        private void AddLine()
+        {
+            Brush_Line();
+            this.pictureBox1.Image = bitmap;
+            this.richTextBox.Text = str;
+        }
+     
+     
+
+        /// <summary>
+        /// 画点
+        /// </summary>
+        /// <param name="p0">PointClass类的点</param>
+        /// <param name="i">计数用的</param>
+        private void DrawPoint(Point p0,string str)
+        {
+            pen = new Pen(Color.Red, 3);
+            graphics.DrawEllipse(pen, p0.X, p0.Y, 1.5f, 1.5f);
+            graphics.DrawString(str, new Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular), new SolidBrush(Color.Blue), p0.X, p0.Y);
+            this.pictureBox1.Image = bitmap;
+        }
+
+        /// <summary>
+        /// 依次传入 两个点 来画线
+        /// 用来画裁剪后的线段
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        private void DrawLine(Point p1, Point p2)
+        {
+            pen = new Pen(Color.Black, 2);
+            //PolylineClass plc = new PolylineClass(pointList.ToArray());
+            //p1 = plc.m_p[plc.m_pointCounts - 1];
+            graphics.DrawLine(pen, p1.X, p1.Y, p2.X, p2.Y);
+           // this.pictureBox1.Image = bitmap;
+        }
+
+
+        /// <summary>
+        /// 生成随机数，返回一个点
+        /// </summary>
+        /// <returns></returns>
+        private Point Random_Point()
+        {
+            //暂时将宽度定义为 600  高度 500
+            Random random = new Random();
+            //int x = random.Next(this.panel_Tools.Width, this.pictureBox1.Width);
+            //int y = random.Next(this.panel_Openfile.Height, this.pictureBox1.Height);
+            int x = random.Next(this.panel_Tools.Width, 600);
+            int y = random.Next(this.panel_Openfile.Height, 500);
+            return new Point(x, y);
+        }
+
+
+            /// <summary>
+        /// 将各个点根据权重连通
+        /// </summary>
+        private  void Brush_Line()
+        {
+            point = PL.ToArray();
+            for (int k = 0; k < PointCounts - 1; k++)
+            {
+                for (int i = k + 1; i < PointCounts; i++)
+                {
+                    if (Metro[k, i] != 2048)
+                    {
+                        DrawLine(point[k], point[i]);
+                        graphics.DrawString(Metro[k, i].ToString(), new Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular), new SolidBrush(Color.Red), (point[k].X + point[i].X) / 2, (point[k].Y + point[i].Y) / 2 );
+                        str = str + "\n" + "点" + (k + 1) + "+点" + (i + 1) + "\t" + Metro[k, i];
+                    }
+                }
+            }
+
+        }
+
+        private void Btu_ShortestPath_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                MessageBox.Show("请先选择文件");
+                return;
+            }
+            init_ShortestPath();
+            FindWay(0);
+            display();
+            this.richTextBox .Text= str;
+            this.pictureBox1.Image = bitmap;
+            
+        }
+
+        /// <summary>
+        /// dijkstra算法的实现部分
+        /// </summary>
+        /// <param name="Start"></param>
+        private void FindWay(int Start)
+        {
+            S.Add(Start);
+            Isfor[Start] = true;
+            //把下标装入U中
+            for (int i = 0; i < PointCounts; i++)
+            {
+                if (i != Start)
+                    U.Add(i);
+            }
+
+            for (int i = 0; i < PointCounts; i++)
+            {
+                distance[i] = Metro[Start, i];
+                prev[i] = 0;
+            }
+            int Count = U.Count;
+            while (Count > 0)
+            {
+                int min_index = (int)U[0]; //假设U中第一个数存储的是最小的数的下标
+                foreach (int r in U)
+                {
+                    if (distance[r] < distance[min_index] && !Isfor[r])
+                        min_index = r;
+                }
+                S.Add(min_index); //S加入这个最近的点
+                Isfor[min_index] = true;
+                U.Remove(min_index); //U中移除这个点；
+                foreach (int r in U)
+                {
+                    //查找下一行邻接矩阵，如何距离和上一个起点的距离和与数组存储的相比比较小，就更改新的距离和起始点,再比对新的起点到各边的距离
+                    if (distance[r] > distance[min_index] + Metro[min_index, r])
+                    {
+                        distance[r] = distance[min_index] + Metro[min_index, r];
+                        prev[r] = min_index;
+                    }
+                    else
+                    {
+                        distance[r] = distance[r];
+                    }
+                }
+                Count = U.Count;
+            }
+        }
+        private void init_ShortestPath()
+        {
+            S=new ArrayList(PointCounts);
+            U=new ArrayList(PointCounts);
+            Isfor = new bool[ PointCounts];
+
+            distance = new int[PointCounts];
+            prev = new int[PointCounts];
+        }
+
+       
+        /// <summary>
+        /// 把生成数据显示出来
+        /// </summary>
+    
+        private void display()
+        {
+            str += "\n";
+            for (int i = 1; i < PointCounts; i++)
+            {
+                str = str + "V1到V"+ (i + 1)+"的最短路径为:V1" ;
+                int prePoint = prev[i];
+                string s = "";
+                StringBuilder sb = new StringBuilder(10);
+                while (prePoint > 0)
+                {
+                    s = (prePoint + 1) + s;
+                    prePoint = prev[prePoint];
+                }
+                int j = 0;
+                for ( j = 0; j < s.Length; j++)
+                {
+                    sb.Append("→V").Append(s[j]);
+                    if (i == PointCounts-1)
+                    {
+                        if (j == 0)
+                            graphics.DrawLine(new Pen(Color.Red, 3), point[0].X, point[0].Y, point[int.Parse(s.Substring(j,1))-1].X, point[int.Parse(s.Substring(j,1))-1].Y);
+                        else if (j<s.Length)
+                            graphics.DrawLine(new Pen(Color.Red, 3), point[int.Parse(s.Substring(j-1, 1))-1].X, point[int.Parse(s.Substring(j-1, 1))-1].Y, point[int.Parse(s.Substring(j , 1))-1].X, point[int.Parse(s.Substring(j , 1))-1].Y);
+                        
+                    }
+                }
+                if (i == PointCounts - 1 && j == s.Length )
+                        graphics.DrawLine(new Pen(Color.Red, 3), point[int.Parse(s.Substring(j-1, 1)) - 1].X, point[int.Parse(s.Substring(j-1, 1)) - 1].Y, point[PointCounts - 1].X, point[PointCounts - 1].Y);
+                str=str+sb;
+                str = str + "→V" + (i + 1);
+                str = str + ":" + distance[i] + "\n";
+            }
+        }
+        
+    }
+}
